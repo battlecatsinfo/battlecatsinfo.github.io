287cf3b73c20f31f3f0e590cf561ca5fc85cdb66
ianfun <015006@mst.hlis.hlc.edu.tw>
2023-07-31 11:19:13 +0800

    Stage info

--- stage.js
+++ stage.js
@@ -11,10 +11,15 @@ const stName2 = document.getElementById('st-name2');
 const stLines = document.getElementById('lines');
 const main_div = document.getElementById('main');
 const search_result = document.getElementById('search-result');
+const m_drops = document.getElementById('drops');
+const rewards = document.getElementById('rewards');
+const m_times = document.getElementById('times');
+const mM = document.getElementById('mM');
 var info1, info2, info3;
 var enemy_names;
 var star;
 var Buffer = BrowserFS.BFSRequire('buffer').Buffer;
+const materialDrops = [85, 86, 87, 88, 89, 90, 91, 140, 187, 188, 189, 190, 191, 192, 193, 194];
 BrowserFS.install(window);
 fetch('enemyName.json').then(res => res.json()).then(json => enemy_names = json).then(function() {
 fetch('/stages.zip').then(res => res.arrayBuffer()).then(function(zipData) {
@@ -106,6 +111,50 @@ function t3str(x) {
   }
   return s;
 }
+function getDropData() {
+  const res = [];
+  var sum = 0;
+  const drops = info3.drop;
+  for (let x of drops)
+    sum += x[0];
+  if (sum == 1000) {
+    for (let x of drops)
+      res.push((x[0] / 10).toFixed(0));
+  } else if ((sum == drops.length && sum != -1) || info3.rand == -3) {
+    for (let x of drops)
+      res.push(x[0].toString());
+    return res;
+  } else if (sum == 100) {
+    for (let x of drops) {
+      res.push(x[0].toString());
+    }
+  } else if (sum > 100 && (info3.rand == 0 || info3.rand == 1)) {
+    var rest = 100;
+    if (drops[0][0] == 100) {
+      res.push('100');
+      for (let i = 1;i < drops.length;++i) {
+        const filter = rest * drops[i][0] / 100;
+        rest -= filter;
+        res.push(filter.toFixed(0));
+      }
+    } else {
+      for (let x of drops) {
+        const filter = rest * x[0] / 100;
+        rest -= filter;
+        res.push(filter.toFixed(0));
+      }
+    }
+  } else if (info3.rand == -4) {
+    for (let x of drops) {
+      res.push((x[0] * 100 / sum).toFixed(0));
+    }
+  } else {
+    for (let x of drops) {
+      res.push(x[0].toString());
+    }
+  }
+  return res;
+}
 M3.oninput = function() {
   const dir = M3.selectedOptions[0].value;
   const url = new URL(location.href);
@@ -118,6 +167,15 @@ M3.oninput = function() {
   const stars_tr = document.getElementById('stars-tr');
   if (stars_tr)
     stars_tr.parentNode.removeChild(stars_tr);
+  const warn_tr = document.getElementById('warn-tr');
+  if (warn_tr)
+    warn_tr.parentNode.removeChild(warn_tr);
+  let mult = info2.stars[star - 1];
+  if (!mult) {
+    mult = 1;
+  } else {
+    mult *= 0.01;
+  }
   if (info2.stars.length) {
     const tr = document.createElement('tr');
     const th = document.createElement('th');
@@ -128,22 +186,117 @@ M3.oninput = function() {
       a.innerText = (i+1).toString() + '★: ' + info2.stars[i].toString() + '%';
       url.searchParams.set('star', i+1);
       a.href = url.href;
+      if ((!star && !i) || (star -1 == i)) {
+        a.style.color = '#a5652a';
+      }
       th.appendChild(a);
     }
     tr.appendChild(th);
     tr.id = 'stars-tr';
     stName.parentNode.parentNode.appendChild(tr);
   }
-  let mult = info2.stars[star - 1];
-  if (!mult) {
-    mult = 1;
-  } else {
-    mult *= 0.01;
+  if (info3.hasOwnProperty('nC') || info2.rM || info1.hasOwnProperty('gc') || info2.hasOwnProperty('gc')) {
+    var s;
+    const tr = document.createElement('tr');
+    const th = document.createElement('th');
+    tr.style.fontSize = '15px';
+    th.colSpan = 6;
+    if (info2.rM) {
+      const span = document.createElement('div');
+      span.innerText = ["過關獎勵將會在再次出現時重置", "清除狀況將會在再次出現時重置", "可過關次數將會在再次出現時重置"][info2.rM - 1];
+      span.style.color = 'brown';
+      th.appendChild(span);
+    }
+    if (info1.hasOwnProperty('gc') || info2.hasOwnProperty('gc')) {
+      const span = document.createElement('div');
+      span.style.color = 'darkgoldenrod';
+      span.innerText = '黃金貓電腦貓電腦掃蕩不可';
+      th.appendChild(span);
+    }
+    if (info3.hasOwnProperty('nC')) {
+      const span = document.createElement('div');
+      span.style.color = '#ff634e';
+      span.innerText = '接關不可';
+      th.appendChild(span);
+    }
+    tr.appendChild(th);
+    tr.id = 'warn-tr';
+    stName.parentNode.parentNode.appendChild(tr);
   }
-  st1[1].innerText = info3.energy;
+  rewards.textContent = '';
+  if (info3.drop.length && M2.selectedOptions[0].value != '/stages/3/3') {
+    const chances = getDropData();
+    for (let i =0;i < info3.drop.length;++i) {
+      if (!parseInt(chances[i])) continue;
+      const v = info3.drop[i];
+      const tr = document.createElement('tr');
+      const td0 = document.createElement('td');
+      const td1 = document.createElement('td');
+      const td2 = document.createElement('td');
+      const rw = v[1];
+      var s = RWSTNAME[rw];
+      if (!s)
+        s = RWNAME[rw];
+      s += ` ×${v[2]}`;
+      td0.innerText = s;
+      td1.appendChild(document.createTextNode(chances[i] + '%' + ((i == 0 && info3.drop[i][0] != 100 && info3.rand != -4) ? '(寶雷)' : '')));
+      td2.innerText = (i == 0 && (info3.rand == 1 || (info3.drop[0][1] >= 1000 && info3.drop[0][1] < 30000))) ? '一次' : '無';
+      tr.appendChild(td0);
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      rewards.appendChild(tr);
+    }
+  }
+  if (rewards.children.length)
+    rewards.parentNode.style.display = 'table';
+  else
+    rewards.parentNode.style.display = 'none';
+  m_drops.textContent = '';
+  for (let i = 1;i < info2.mD.length;++i) {
+    if (info2.mD[i] <= 0)
+      continue;
+    const tr = document.createElement('tr');
+    const td0 = document.createElement('td');
+    td0.innerText = RWNAME[materialDrops[i - 1]];
+    tr.appendChild(td0);
+    const td1 = document.createElement('td');
+    td1.innerText = `${info2.mD[i]}%`;
+    tr.appendChild(td1);
+    m_drops.appendChild(tr);
+  }
+  if (m_drops.children.length)
+    m_drops.parentNode.style.display = 'table';
+  else
+    m_drops.parentNode.style.display = 'none';
+  mM.innerText = `機率(最大掉落量${~~Math.round(mult * info3.mM)})`;
+  if (info3.time.length) {
+    times.textContent = '';
+    for (let tm of info3.time) {
+      const tr = document.createElement('tr');
+      const td0 = document.createElement('td');
+      const td1 = document.createElement('td');
+      const td2 = document.createElement('td');
+      var s = '';
+      const rw = tm[1];
+      var s = RWSTNAME[rw];
+      if (!s)
+        s = RWNAME[rw];
+      td0.innerText = s;
+      td1.innerText = tm[2];
+      td2.innerText = tm[0];
+      tr.appendChild(td0);
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      times.appendChild(tr);
+    }
+    times.parentNode.style.display = 'table';
+  } else {
+    times.parentNode.style.display = 'none';
+  }
+  st1[1].innerText = (M1.selectedOptions[0].value == '/stages/14') ? ('貓力達' + String.fromCharCode(65 + info3.e/1000) + '×' + (info3.e % 1000).toString()) : info3.e;
   st1[3].innerText = info3.max;
   st1[5].innerText = info3.len;
-  st2[1].innerText = info3.health;
+  st2[1].innerText = info3.H;
   const ca = document.createElement('a');
   const cas = t3str(info3.castle % 1000);
   const cad = ["rc", "ec", "wc", "sc"][Math.floor(info3.castle / 1000)];
@@ -158,17 +311,17 @@ M3.oninput = function() {
   st2[5].appendChild(a);
   st3[1].innerText = info3.xp;
   const a1 = document.createElement('a');
-  a1.innerText = t3str(info3.mus0);
-  a1.href = 'https://github.com/battlecatsultimate/bcu-assets/raw/master/music/' + t3str(info3.mus0) + '.ogg';
+  a1.innerText = t3str(info3.m0);
+  a1.href = 'https://github.com/battlecatsultimate/bcu-assets/raw/master/music/' + t3str(info3.m0) + '.ogg';
   const a2 = document.createElement('a');
-  a2.innerText = t3str(info3.mus1);
-  a2.href = 'https://github.com/battlecatsultimate/bcu-assets/raw/master/music/' + t3str(info3.mus1) + '.ogg';
+  a2.innerText = t3str(info3.m1);
+  a2.href = 'https://github.com/battlecatsultimate/bcu-assets/raw/master/music/' + t3str(info3.m1) + '.ogg';
   st3[3].textContent = '';
   st3[3].appendChild(a1);
   st3[5].textContent = '';
   st3[5].appendChild(a2);
   stLines.textContent = '';
-  for (let line of info3.lines) {
+  for (let line of info3.l) {
     const tr = document.createElement('tr');
     const enemy = line[0];
     makeTd(tr, enemy_names[enemy]);
