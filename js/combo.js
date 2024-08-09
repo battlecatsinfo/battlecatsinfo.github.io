f7dd07873c0438f093b97b9d02a056088fd79328
Danny Lin <danny0838@gmail.com>
2024-08-10 07:46:35 +0800

    improve templating for combos
    - Replace combos.html with a combos.json as source data.

--- js/combo.js
+++ js/combo.js
@@ -1,7 +1,16 @@
 module.exports = class extends require('./base.js') {
 	run() {
-		this.write_template('html/combos.html', 'combos.html', {
-			'combos': this.load('combos.html')
-		});
+		const combos = JSON.parse(this.load('combos.json'));
+
+		// fill dummy units to 5
+		for (const type in combos) {
+			for (const combo of combos[type]) {
+				for (let i = 0; i < 5; i++) {
+					if (!combo.units[i]) combo.units[i] = null;
+				}
+			}
+		}
+
+		this.write_template('html/combos.html', 'combos.html', {combos});
 	}
 };
