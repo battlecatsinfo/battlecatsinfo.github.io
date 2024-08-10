94ecb31f63f76c7031c67ee2f1e0ab20fb70bcaa
Danny Lin <danny0838@gmail.com>
2024-08-11 01:51:23 +0800

    improve templating for medal/rank

--- js/rank.js
+++ js/rank.js
@@ -1,13 +1,6 @@
 module.exports = class extends require('./base.js') {
 	run() {
-		this.write_template('html/rank.html', 'rank.html', {
-			'rank': this.load('rank.tsv').split('\n').map(function(x) {
-				if (x.length == 0 || x[0] == 'r') return '';
-				x = x.split('\t');
-				return x[3] ? 
-				`<tr><td>${x[0]}</td><td>${x[1]}</td><td>${x[2]}</td><td><img width="128" height="128" loading="lazy" src="${x[3]}"></td></tr>` : 
-				`<tr><td>${x[0]}</td><td>${x[1]}</td><td>${x[2]}</td><td></td></tr>`;
-			}).join('\n')
-		});
+		const ranks = this.parse_tsv(this.load('rank.tsv'));
+		this.write_template('html/rank.html', 'rank.html', {ranks});
 	}
 };
