9d3532dfe7bebd6d1d6d35c957b78c0c4c40d136
Danny Lin <danny0838@gmail.com>
2024-08-11 17:41:48 +0800

    optimize code for gacha
    - Pass CSS text rather than a raw HTML string for image style.
    - Pass an object rather than a raw HTML string for the reward name.
    - Pass an array of cat objects rather than a joined cat name string.

--- js/gacha.js
+++ js/gacha.js
@@ -2,8 +2,7 @@ const Fraction = require('fraction.js');
 const fs = require('node:fs');
 const resolve = require('node:path').resolve;
 
-const
-category_set = {
+const category_set = {
 	'常駐稀有貓': new Set([37, 38, 41, 46, 47, 48, 49, 50, 51, 52, 55, 56, 58, 145, 146, 147, 148, 149, 197, 198, 308, 325, 376, 495, 523]),
 	'常駐激稀有貓': new Set([30, 31, 32, 33, 35, 36, 39, 40, 61, 150, 151, 152, 153, 199, 307, 377, 522]),
 	'超激烈爆彈':new Set([42, 43, 44, 57, 59, 143, 427, 519, 617, 668, 763]),
@@ -30,8 +29,8 @@ category_set = {
 	"降臨和漩渦關卡掉落": new Set([60, 78, 88, 126, 154, 201, 324, 379, 382, 442, 452, 507, 521, 527, 528, 531, 539, 545, 553, 581, 621, 623, 630, 260, 267, 284, 287, 273, 708, 718]),
 	"遠古的蛋": new Set([656, 658, 659, 663, 664, 665, 669, 670, 675, 676, 685, 691, 697, 700, 706, 707, 713, 716, 717, 720, 724, 730]),
 	"月份貓": new Set([79, 80, 81, 100, 104, 109, 122, 128, 132, 63, 70, 74])
-},
-reward_order = {
+};
+const reward_order = {
 	0: 0,
 	1: 1,
 	2: 2,
@@ -75,8 +74,8 @@ reward_order = {
 	78: 362,
 	159: 358,
 	197: 365
-},
-reward_names = {
+};
+const reward_names = {
 	0: '加速',
 	1: '寶物雷達',
 	2: '土豪貓',
@@ -136,15 +135,16 @@ module.exports = class extends require('./base.js') {
 		}
 
 		const gacha_template = this.load_a('html/gacha.html');
-		let size, width, height, path, data, gachas = [];
+		const gachas = [];
 
 		this.fmt = new Intl.NumberFormat('zh-Hant', { maximumFractionDigits: 5 });
 		this.load_unit();
 
 		for (const pool of JSON.parse(this.load('pools.json'))) {
-			size = pool['size'];
-			[width, height] = size ? size.split('x') : [860, 240];
+			const size = pool['size'];
+			const [width, height] = size ? size.split('x') : [860, 240];
 
+			let data;
 			switch (pool['type']) {
 			case 'category':
 				data = this.get_category(pool);
@@ -163,7 +163,7 @@ module.exports = class extends require('./base.js') {
 				console.assert(false);
 			}
 
-			path = 'gacha/' + to_path(pool['en-name']) + '.html';
+			const path = 'gacha/' + to_path(pool['en-name']) + '.html';
 
 			this.write_string(path, this.template(
 				gacha_template,
@@ -193,19 +193,18 @@ module.exports = class extends require('./base.js') {
 		return this.egg_set.has(u) ? `/img/u/${u}/2.png` : `/img/u/${u}/0.png`;
 	}
 	get_rare(O) {
-		const
-			self = this,
-			units = O['units'],
-			rarity_desc = [
+		const self = this;
+		const units = O['units'];
+		const rarity_desc = [
 				"P.S. 基本貓於Lv60後成長幅度減半",
 				"P.S. EX貓於Lv60後成長幅度減半",
 				"P.S. 稀有貓於Lv70後成長幅度減半，於Lv90後成長幅度再次減半",
 				"P.S. 激稀有貓於Lv60後成長幅度減半，於Lv80後成長幅度再次減半",
 				"P.S. 超激稀有貓於Lv60後成長幅度減半，於Lv80後成長幅度再次減半",
 				"P.S. 傳說稀有貓於Lv60後成長幅度減半，於Lv80後成長幅度再次減半"
-			],
-			colors = ['#d0e0e3', '#d9d2e9', '#c9daf8', '#fce5cd'];
-		let S = {
+		];
+		const colors = ['#d0e0e3', '#d9d2e9', '#c9daf8', '#fce5cd'];
+		const S = {
 			rarity_descs: Array.from(new Set(units.map(x => rarity_desc[self.unit_rarity[x]]))),
 			summary: [],
 			item_free: null,
@@ -213,21 +212,13 @@ module.exports = class extends require('./base.js') {
 			item_minor: [],
 		};
 		for (let rarity = 5;rarity >= 2;--rarity) {
-			let c = 0;
-
-			for (const u of units) {
-				if (this.unit_rarity[u] == rarity)
-					++c;
-			}
-
+			let c = units.filter(u => this.unit_rarity[u] === rarity).length;
 			let x = rarity - 2;
 			let rate = O['rate'][x];
 			if (rate) {
-				let color = colors[x];
-				let name = ['稀有', '激稀有', '超激稀有', '傳說稀有'][x];
 				S.summary.push({
 					bgColor: colors[x],
-					name,
+					name: ['稀有', '激稀有', '超激稀有', '傳說稀有'][x],
 					rate: `${rate / 100}%`,
 					total: c,
 					rateEach: `${this.fmt.format(rate / (100 * c))}%`,
@@ -241,33 +232,36 @@ module.exports = class extends require('./base.js') {
 				descs: this.unit_desc[O['free']].split('|'),
 			};
 		}
-		let MUL = {};
+		const MUL = {};
 		for (let rarity = 5;rarity >= 0;--rarity) {
-			let out = {'0': []};
-			outer: for (const u of units) {
-				if (this.unit_rarity[u] == rarity) {
-					if (MUL[u]) {
-						MUL[u] += 1;
-						continue;
-					} else {
-						MUL[u] = 1;
-					}
-
-					for (const [k, v] of Object.entries(category_set)) {
-						if (k != O['tw-name'] && v.has(u)) {
-							if (out[k]) {
-								out[k].push(u);
-							} else {
-								out[k] = [u];
-							}
-							continue outer;
-						}
-					}
-
-					out['0'].push(u);
+			const out = {'0': []};
+			outer:
+			for (const u of units) {
+				if (this.unit_rarity[u] !== rarity) {
+					continue;
 				}
+
+				if (MUL[u]) {
+					MUL[u] += 1;
+					continue;
+				} else {
+					MUL[u] = 1;
+				}
+
+				for (const [name, idSet] of Object.entries(category_set)) {
+					if (name !== O['tw-name'] && idSet.has(u)) {
+						if (out[name]) {
+							out[name].push(u);
+						} else {
+							out[name] = [u];
+						}
+						continue outer;
+					}
+				}
+
+				out['0'].push(u);
 			}
-			let v = out['0'];
+			const v = out['0'];
 			delete out['0'];
 
 			if (v.length) {
@@ -276,29 +270,28 @@ module.exports = class extends require('./base.js') {
 					name: ['基本', 'EX', '稀有', '激稀有', '超激稀有', '傳說稀有'][rarity],
 					rows: [],
 				});
-				let i = 0;
 				v.sort((a, b) => a - b);
-				while (i < (v.length - 1)) {
+				for (let i = 0, I = v.length - 1; i < I; i += 2) {
 					S.item_groups.at(-1).rows.push([
 						this.get1(v[i], MUL[v[i]]),
 						this.get1(v[i + 1], MUL[v[i + 1]]),
 					]);
-					i += 2;
 				}
 
 				if (v.length & 1) {
-					let u = v[v.length - 1];
+					const u = v[v.length - 1];
 					S.item_groups.at(-1).rows.push([
 						this.get1(u, MUL[u]),
 					]);
 				}
 			}
 
-			for (const [k, v] of Object.entries(out))
+			for (const [name, ids] of Object.entries(out)) {
 				S.item_minor.push({
-					name: k,
-					text: v.sort((a, b) => a - b).map(x => self.unit_name[x]).join('、'),
+					name,
+					cats: ids.sort((a, b) => a - b).map(id => ({id, name: self.unit_name[id]})),
 				});
+			}
 		}
 		return S;
 	}
@@ -312,13 +305,12 @@ module.exports = class extends require('./base.js') {
 		return c;
 	}
 	get_normal(O) {
-		let I, S = [];
-		const
-			units = O['units'],
-			result = [],
-			tech_names = ['貓咪砲攻擊力', '貓咪砲射程','貓咪砲充電','工作狂貓的工作效率','工作狂貓錢包','城堡體力','研究力','會計能力','學習力','統率力'],
-			tech_links = ['TvNbDkW', 'xzUGr54', 'NwhDVgh', '1Uuos4y', 'DKyMbmd', 'UP3sYQ5', 'kClFt3L','lwKJIlC', 'osFMsUn', 'eToHyXp'];
-		for (let i = 0;i < 5;++i) {
+		const S = [];
+		const units = O['units'];
+		const result = [];
+		const tech_names = ['貓咪砲攻擊力', '貓咪砲射程','貓咪砲充電','工作狂貓的工作效率','工作狂貓錢包','城堡體力','研究力','會計能力','學習力','統率力'];
+		const tech_links = ['TvNbDkW', 'xzUGr54', 'NwhDVgh', '1Uuos4y', 'DKyMbmd', 'UP3sYQ5', 'kClFt3L','lwKJIlC', 'osFMsUn', 'eToHyXp'];
+		for (let i = 0, I;i < 5;++i) {
 			let rate = O['rate'][i];
 			if (!rate)
 				continue;
@@ -334,12 +326,12 @@ module.exports = class extends require('./base.js') {
 						-1,
 						I + 667, 
 						this.uimg(I),
-						`<a target="_blank" href="/unit.html?id=${I}">${this.unit_name[I]}</a>`,
+						{id: I, name: this.unit_name[I]},
 						r, 
 						'',
 						'104',
 						'79',
-						'style="padding:.4em" '
+						'padding:.4em'
 					]);
 					break;
 				case 1:
@@ -348,7 +340,7 @@ module.exports = class extends require('./base.js') {
 						~~(I / 100),
 						I,
 						`/img/r/${x[1]}.png`,
-						reward_names[x[1]],
+						{name: reward_names[x[1]]},
 						r,
 						'',
 						'128',
@@ -362,7 +354,7 @@ module.exports = class extends require('./base.js') {
 						-2,
 						I,
 						`https://i.imgur.com/${tech_links[I]}.png`,
-						tech_names[I],
+						{name: tech_names[I]},
 						r,
 						'',
 						'128',
@@ -378,12 +370,11 @@ module.exports = class extends require('./base.js') {
 			return a[1] - b[1]
 		});
 
-		let
-			e_type = result[0][0],
-			count = 0,
-			last_i = 0,
-			rate = Fraction(0),
-			color = 0;
+		let e_type = result[0][0];
+		let count = 0;
+		let last_i = 0;
+		let rate = Fraction(0);
+		let color = 0;
 
 		for (let i = 0;i < result.length;++i) {
 			const v = result[i];
@@ -417,7 +408,7 @@ module.exports = class extends require('./base.js') {
 				imgSrc: x[2],
 				imgWidth: x[6],
 				imgHeight: x[7],
-				name: x[3],
+				info: x[3],
 				rate: `${this.fmt.format(x[4])}%`,
 				rateSum: x[5],
 			});
@@ -425,20 +416,17 @@ module.exports = class extends require('./base.js') {
 		return S;
 	}
 	get_event(O) {
-		const
-			units = O['units'],
-			d_rate = O['rate'] || [0,0,0,0,0,0,0,0,0,0],
-			result = [];
-		let
-			I,
-			S = {
+		const S = {
 				must_drop_rate: null,
 				items: [],
-			},
-			must_drop_group = 0,
-			must_drop_rate = 0;
+		};
+		const units = O['units'];
+		const d_rate = O['rate'] || [0,0,0,0,0,0,0,0,0,0];
+		const result = [];
+		let must_drop_group = 0;
+		let must_drop_rate = 0;
 
-		for (let i = 0;i < 9;i += 2) {
+		for (let i = 0, I;i < 9;i += 2) {
 			let rate = d_rate[i];
 			let group = units[i >> 1];
 			let R = new Fraction(rate, group.length || 1);
@@ -455,13 +443,13 @@ module.exports = class extends require('./base.js') {
 						-1,
 						I + 667,
 						this.uimg(I),
-						`<a target="_blank" href="/unit.html?id=${I}">${this.unit_name[I]}</a>${must}`,
+						{id: I, name: this.unit_name[I], must},
 						r,
 						null,
 						rate,
 						'104',
 						'79',
-						'style="padding:.4em" '
+						'padding:.4em'
 					]);
 				} else {
 					I = reward_order[x];
@@ -469,7 +457,7 @@ module.exports = class extends require('./base.js') {
 						~~(I / 100),
 						I,
 						`/img/r/${x}.png`,
-						reward_names[x],
+						{name: reward_names[x]},
 						r,
 						null,
 						rate,
@@ -482,12 +470,11 @@ module.exports = class extends require('./base.js') {
 		}
 
 		result.sort((a, b) => a[1] - b[1]);
-		let
-			e_type = result[0][0],
-			count = 0,
-			last_i = 0,
-			rate = new Fraction(0),
-			color = 0;
+		let e_type = result[0][0];
+		let count = 0;
+		let last_i = 0;
+		let rate = new Fraction(0);
+		let color = 0;
 
 		for (let i = 0;i < result.length;++i) {
 			const v = result[i];
@@ -515,7 +502,7 @@ module.exports = class extends require('./base.js') {
 			const r = v[4].n ? this.fmt.format(v[4].valueOf() / 100) + '%' : 'N/A';
 			if (must_drop_rate) {
 				let a;
-				if (v[3].endsWith('*'))
+				if (v[3].must)
 					a = (0.9 * (v[6] / 100) + 10) / must_drop_group.length;
 				else 
 					a = v[4].mul(must_drop_rate).valueOf() / 1000000;
@@ -525,7 +512,7 @@ module.exports = class extends require('./base.js') {
 					imgSrc: v[2],
 					imgWidth: v[7],
 					imgHeight: v[8],
-					name: v[3],
+					info: v[3],
 					rate: r,
 					rateSum: v[5],
 					rateActual: a ? this.fmt.format(a) + '%' : 'N/A',
@@ -537,7 +524,7 @@ module.exports = class extends require('./base.js') {
 					imgSrc: v[2],
 					imgWidth: v[7],
 					imgHeight: v[8],
-					name: v[3],
+					info: v[3],
 					rate: r,
 					rateSum: v[5],
 				});
@@ -549,13 +536,12 @@ module.exports = class extends require('./base.js') {
 		return S;
 	}
 	get_category(O) {
-		const iter = Array.from(category_set[O['category']]).sort((a, b) => a - b)[Symbol.iterator]();
-		let
-			f,
-			c = 0, 
-			S = [];
+		const S = [];
+		const ids = Array.from(category_set[O['category']]).sort((a, b) => a - b);
+		let f;
+		let c = 0;
 
-		for (let u of iter) {
+		for (const u of ids) {
 			c++;
 			if (c & 1) 
 				f = u;
@@ -568,13 +554,13 @@ module.exports = class extends require('./base.js') {
 
 		return S;
 	}
-	get1(u, C = 1) {
+	get1(u, rate = 1) {
 		return {
 			id: u,
 			name: this.unit_name[u],
 			descs: this.unit_desc[u].split('|'),
 			img: this.uimg(u),
-			rate: C !== 1 ? C : undefined,
+			rate: rate !== 1 ? rate : undefined,
 		}
 	}
 	load_unit() {
