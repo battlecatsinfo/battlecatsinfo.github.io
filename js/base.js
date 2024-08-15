224fb8d8f0020e978631e74bad3d56720340ba0f
Danny Lin <danny0838@gmail.com>
2024-08-15 09:07:26 +0800

    introduce "op" and common helpers
    - Remove "ifCond" helper.

--- js/base.js
+++ js/base.js
@@ -15,37 +15,118 @@ for (const file of fs.readdirSync(layoutDir)) {
 	);
 }
 
-Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
-	switch (operator) {
-		case '==':
-			return (v1 == v2) ? options.fn(this) : options.inverse(this);
-		case '===':
-			return (v1 === v2) ? options.fn(this) : options.inverse(this);
-		case '!=':
-			return (v1 != v2) ? options.fn(this) : options.inverse(this);
-		case '!==':
-			return (v1 !== v2) ? options.fn(this) : options.inverse(this);
-		case '<':
-			return (v1 < v2) ? options.fn(this) : options.inverse(this);
-		case '<=':
-			return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-		case '>':
-			return (v1 > v2) ? options.fn(this) : options.inverse(this);
-		case '>=':
-			return (v1 >= v2) ? options.fn(this) : options.inverse(this);
-		case '&&':
-			return (v1 && v2) ? options.fn(this) : options.inverse(this);
-		case '||':
-			return (v1 || v2) ? options.fn(this) : options.inverse(this);
-		default:
-			return options.inverse(this);
-	}
-});
+/**
+ * Register helpers for common operations.
+ *
+ * @example
+ * {{and mycond1 mycond2 mycond3}}
+ * {{or mycond1 mycond2 mycond3}}
+ * {{not mycond}}
+ * {{typeof myvar}}
+ * {{match myvar "go+le" "i"}}
+ * {{sum mynum1 mynum2 mynum3}}
+ */
+{
+	const namedOperators = {
+		'and': function (...args) { return args.reduce((rv, x) => rv && x); },
+		'or': function (...args) { return args.reduce((rv, x) => rv || x); },
+		'not': function (x) { return !x; },
+		'typeof': function (x) { return typeof x; },
+		'match': function (str, regex, flags) {
+			if (!(regex instanceof RegExp)) {
+				regex = new RegExp(regex, flags);
+			}
+			return str.match(regex);
+		},
+		'sum': function (...args) { return args.reduce((rv, x) => rv + x); },
+	};
 
-Handlebars.registerHelper('sum', function (...args) {
-	const options = args.pop();
-	return args.reduce((sum, n) => sum + parseInt(n, 10));
-});
+	for (const op in namedOperators) {
+		const fn = namedOperators[op];
+		Handlebars.registerHelper(op, function (...args) {
+			const options = args.pop();
+			return fn.apply(this, args);
+		});
+	}
+}
+
+/**
+ * Register "op" helper.
+ *
+ * @example
+ * {{op myvar "==" "somestr"}}
+ * {{op "foo" "in" "foobar"}}
+ * {{op "!" myvar}}
+ */
+{
+	const unaryOperators = {
+		'!': function (x) { return !x; },
+		'~': function (x) { return ~x; },
+		'typeof': function (x) { return typeof x; },
+	};
+	const binaryOperators = {
+		'&&': function (v1, v2) { return v1 && v2; },
+		'||': function (v1, v2) { return v1 || v2; },
+
+		'==': function (v1, v2) { return v1 == v2; },
+		'===': function (v1, v2) { return v1 === v2; },
+		'!=': function (v1, v2) { return v1 != v2; },
+		'!==': function (v1, v2) { return v1 !== v2; },
+		'<': function (v1, v2) { return v1 < v2; },
+		'<=': function (v1, v2) { return v1 <= v2; },
+		'>': function (v1, v2) { return v1 > v2; },
+		'>=': function (v1, v2) { return v1 >= v2; },
+
+		'+': function (v1, v2) { return v1 + v2; },
+		'-': function (v1, v2) { return v1 - v2; },
+		'*': function (v1, v2) { return v1 * v2; },
+		'/': function (v1, v2) { return v1 / v2; },
+		'%': function (v1, v2) { return v1 % v2; },
+		'**': function (v1, v2) { return v1 ** v2; },
+
+		'&': function (v1, v2) { return v1 & v2; },
+		'|': function (v1, v2) { return v1 | v2; },
+		'^': function (v1, v2) { return v1 ^ v2; },
+		'<<': function (v1, v2) { return v1 << v2; },
+		'>>': function (v1, v2) { return v1 >> v2; },
+		'>>>': function (v1, v2) { return v1 >>> v2; },
+
+		'instanceof': function (v1, v2) { return v1 instanceof v2; },
+
+		// Object, Array, String, Set, Map
+		'in': function (v1, v2) {
+			if (Array.isArray(v2) || typeof v2 === 'string') {
+				return v2.includes(v1);
+			}
+			if (v2 instanceof Set || v2 instanceof Map) {
+				return v2.has(v1);
+			}
+			return v2.hasOwnProperty(v1);
+		},
+	};
+
+	Handlebars.registerHelper('op', function (...args) {
+		const options = args.pop();
+
+		// e.g. {{op "!" myvar}}
+		if (args.length === 2) {
+			const [op,  v1] = args;
+			const fn = unaryOperators[op];
+			if (!fn) {
+				throw new Error(`Unknown unary operator ${JSON.stringify(op)}`);
+			}
+			return fn.call(this, v1);
+		}
+
+		// e.g. {{op myvar "==" "somestr"}}
+		const [v1, op, v2] = args;
+		const fn = binaryOperators[op];
+		if (!fn) {
+			throw new Error(`Unknown operator ${JSON.stringify(op)}`);
+		}
+		return fn.call(this, v1, v2);
+	});
+}
 
 Handlebars.registerHelper('toJSON', function (obj, space) {
 	return JSON.stringify(obj, null, space);
