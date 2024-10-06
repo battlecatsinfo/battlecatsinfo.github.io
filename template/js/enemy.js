08ae000a3ef889e5f1df55c393ca43974484bb3c
battlecatsinfo <ianfun.github.io@gmail.com>
2024-10-06 21:09:41 +0800

    add dpsgraph link to enemy dropdown

--- template/js/enemy.js
+++ template/js/enemy.js
@@ -645,4 +645,5 @@ loadEnemy(my_id)
 		addBarItem('搜尋出現關卡', search_for);
 		addBarItem('檢視動畫', E.animUrl);
 		addBarItem('金寶科技設定', '/settings.html#treasure');
+		addBarItem('DPS 曲線', `/dpsgraph_svg.html?units=${E.id}`);
 	});
