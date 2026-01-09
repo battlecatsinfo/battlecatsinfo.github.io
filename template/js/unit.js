508bcd9b20f1429666651b9de7479beb5e96f250
- <141103843+drocisd@users.noreply.github.com>
2026-01-09 22:20:40 +0800

    add back missing variable for uf

--- template/js/unit.js
+++ template/js/unit.js
@@ -94,6 +94,7 @@ var my_cat;
 var lvMax;
 var tf_tbl;
 var tf_tbl_s;
+var tf4_raw_tbl;
 var tf4_tbl;
 var super_talent = false;
 var custom_talents;
@@ -2216,7 +2217,10 @@ function renderUnitPage() {
 			}
 			const tbl = renderForm(form, baseForm = my_cat.forms[form.lvc]);
 			if (form.lvc == 2) tf_tbl_s = tbl;
-			else if (form.lvc == 3) tf4_tbl = tbl;
+			else if (form.lvc == 3) {
+				tf4_raw_tbl = tbl;
+				tf4_tbl = tbl;
+			}
 			tables.push([
 				['一', '二', '三', '四'][form.lvc] + "階數值表格", tbl
 			]);
